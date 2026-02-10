d6b1be45f9f979279a612eb6cc19f55aef5fe6ef
Avic <173277100+developerAvic@users.noreply.github.com>
2026-02-10 12:40:55 +0530

    Update queues fifo principle - program 4.c

--- queues fifo principle - program 4.c
+++ queues fifo principle - program 4.c
@@ -1,56 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *q;
-int max, rear = -1, front = 0, count = 0;
+int *q; // Pointer to dynamically allocated array that stores queue elements.
+int max, 
+rear = -1, // Points to last inserted element. Starts at -1 because queue is empty initially. 
+front = 0, 
+count = 0; // Tracks number of elements present. This variable is extremely important because circular queues cannot rely only on front and rear to detect empty/full states safely.
 
-void Enqueue()
-{
+
+// if queue is full say its full
+//  else 
+    // read the element to be inserted
+    // When rear reaches last index and increments, modulo wraps it back to zero (i.e the first position of the queue).
+    // insert element at the new rear position.
+    // increment the element count.
+    
+void Enqueue(){
     int ele;
 
     if (count == max)
         printf("Queue overflow\n");
-    else
-    {
+    else{
         printf("Enter the element to be inserted\n");
         scanf("%d", &ele);
-        rear = (rear + 1) % max;
+        rear = (rear + 1) % max; // When rear reaches last index and increments, modulo wraps it back to zero (i.e the first position of the queue) .
         q[rear] = ele;
         count++;
     }
 }
 
-void Dequeue()
-{
+
+// if elements in queue is empty say its empty
+// else 
+    // display the about to be removed element i.e the first element in the queue
+    // increase the front variable so tht the next element becomes the front
+    // decrease the element count value
+
+void Dequeue(){
     if (count == 0)
         printf("Queue underflow\n");
-    else
-    {
+    else{
         printf("Deleted element is %d\n", q[front]);
         front = (front + 1) % max;
         count--;
     }
 }
 
-void display()
-{
+
+// if queue is empty say its empty
+// else 
+    // traverse the queue
+        // point to the first element in queue to an variable k
+        // run the for loop only for the number of elements present in queue rather than the max queue size. 
+            // print the values of queue using k.
+            // Moves index forward while wrapping around.
+
+void display(){
     int i, k;
 
     if (count == 0)
         printf("Queue is empty\n");
-    else
-    {
-        k = front;
-        for (i = 0; i < count; i++)
-        {
+    else{
+        k = front; // point to the first element in queue
+        for (i = 0; i < count; i++){ // run the for loop only for the number of elements present in queue rather than the max queue size. 
             printf("%d\n", q[k]);
-            k = (k + 1) % max;
+            k = (k + 1) % max; // Moves index forward while wrapping around.
         }
     }
 }
 
-int main()
-{
+
+// get the max size of the queue
+// allocate memory for the queue based on max 
+// while loop with choice and switch statement for all 3 functions and exit.
+
+int main(){
     int choice, ele;
 
     printf("Enter the size of the queue: ");
@@ -58,14 +82,12 @@ int main()
 
     q = (int *)malloc(max * sizeof(int));
 
-    while (1)
-    {
+    while (1){
         printf("\nEnter your choice\n");
         printf("1 for enqueue\n2 for dequeue\n3 for display\n4 for exit\n");
         scanf("%d", &choice);
 
-        switch (choice)
-        {
+        switch (choice){
             case 1: Enqueue(); break;
             case 2: Dequeue(); break;
             case 3: display(); break;
