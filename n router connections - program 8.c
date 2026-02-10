024bfba98963b3a59824d020eb3d6ded129cce4a
Avic <173277100+developerAvic@users.noreply.github.com>
2026-02-10 20:06:40 +0530

    Update n router connections - program 8.c

--- n router connections - program 8.c
+++ n router connections - program 8.c
@@ -1,143 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <ctype.h>
 
-#define MAX 50
+#define MAX 100
 
-char istack[MAX];
-int tos = -1;
+int dfn[MAX], low[MAX], parent[MAX];
+int visited[MAX], articulation[MAX];
+int time_counter = 0;  // Global timer for discovery and low values
 
-float stack[MAX];
-int top = -1;
-
-/* ---------- Operator Stack ---------- */
-
-void ipush(char s) {
-    if (tos == MAX - 1)
+void dfnlow(int u, int adj[MAX][MAX], int V)
+{
+    int children = 0;
+    int v;
+    
+    visited[u] = 1;
+    dfn[u] = low[u] = ++time_counter;
+    
+    for (v = 0; v < V; v++)
     {
-        printf("Operator stack overflow\n");
-        exit(1);
-    }
-    istack[++tos] = s;
-}
-
-char ipop() {
-    if (tos == -1)
-    {
-        printf("Operator stack underflow\n");
-        exit(1);
-    }
-    return istack[tos--];
-}
-
-int precd(char s) {
-    switch (s)
-    {
-        case '^': return 4;
-        case '*':
-        case '%':
-        case '/': return 3;
-        case '+':
-        case '-': return 2;
-        case '(' : return 1;
-    }
-    return 0;
-}
-
-/* ---------- Infix to Postfix ---------- */
-
-void convertip(char infix[], char postfix[]) {
-    int i, j = 0;
-    char symbol;
-
-    ipush('(');
-    strcat(infix, ")");
-
-    for (i = 0; infix[i] != '\0'; i++) {
-        symbol = infix[i];
-
-        if (isalnum(symbol)) {
-            postfix[j++] = symbol;
-        }
-        else if (symbol == '(') {
-            ipush(symbol);
-        }
-        else if (symbol == ')') {
-            while (istack[tos] != '(')
-                postfix[j++] = ipop();
-            ipop();
-        }
-        else {
-            while (precd(symbol) <= precd(istack[tos]))
-                postfix[j++] = ipop();
-            ipush(symbol);
-        }
-    }
-
-    postfix[j] = '\0';
-}
-
-/* ---------- Operand Stack ---------- */
-
-void push(float ele) {
-    if (top == MAX - 1) {
-        printf("Evaluation stack overflow\n");
-        exit(1);
-    }
-    stack[++top] = ele;
-}
-
-float pop() {
-    if (top == -1) {
-        printf("Evaluation stack underflow\n");
-        exit(1);
-    }
-    return stack[top--];
-}
-
-/* ---------- Postfix Evaluation ---------- */
-
-void evaluate(char postfix[]){
-    float op1, op2, x;
-
-    for (int i = 0; postfix[i] != '\0'; i++){
-        char sym = postfix[i];
-
-        if (isalpha(sym)) {
-            printf("Enter value of %c: ", sym);
-            scanf("%f", &x);
-            push(x);
-        }
-        else {
-            op2 = pop();
-            op1 = pop();
-
-            switch (sym){
-                case '+': push(op1 + op2); break;
-                case '-': push(op1 - op2); break;
-                case '*': push(op1 * op2); break;
-                case '/': push(op1 / op2); break;
-                case '%': push((int)op1 % (int)op2); break;
-                case '^': push(pow(op1, op2)); break;
-                default:
-                    printf("Invalid postfix expression\n");
-                    exit(1);
+        if (adj[u][v])  // If there is an edge from u to v
+        {
+            if (!visited[v])  // v is not visited (tree edge)
+            {
+                children++;
+                parent[v] = u;
+                dfnlow(v, adj, V);
+                
+                // Update low value of u with low value of child v
+                if (low[u] > low[v])
+                    low[u] = low[v];
+                
+                // Check for articulation point conditions
+                // 1. Root node with at least two children
+                if (parent[u] == -1 && children > 1)
+                    articulation[u] = 1;
+                
+                // 2. Non-root node where no child has back edge to ancestor
+                if (parent[u] != -1 && low[v] >= dfn[u])
+                    articulation[u] = 1;
+            }
+            else if (v != parent[u])  // Back edge (not to immediate parent)
+            {
+                // Update low value using discovery time of ancestor
+                if (low[u] > dfn[v])
+                    low[u] = dfn[v];
             }
         }
     }
-    printf("Result = %.2f\n", pop());
 }
 
+void findArticulationPoints(int adj[MAX][MAX], int V)
+{
+    int i;
+    
+    // Initialize all arrays
+    for (i = 0; i < V; i++)
+    {
+        parent[i] = -1;
+        visited[i] = 0;
+        articulation[i] = 0;
+        dfn[i] = low[i] = 0;
+    }
+    
+    time_counter = 0;
+    
+    // Handle possibly disconnected graph
+    for (i = 0; i < V; i++)
+    {
+        if (!visited[i])
+            dfnlow(i, adj, V);
+    }
+    
+    // Print results
+    printf("\nArticulation points (Critical Routers) are:\n");
+    int found = 0;
+    for (i = 0; i < V; i++)
+    {
+        if (articulation[i])
+        {
+            printf("Router %d\n", i);
+            found = 1;
+        }
+    }
+    
+    if (!found)
+        printf("No articulation points found.\n");
+}
 
-int main(){
-    char infix[MAX], postfix[MAX];
-    printf("Enter infix expression: ");
-    fgets(infix, MAX, stdin);
-    infix[strcspn(infix, "\n")] = '\0';
-    convertip(infix, postfix);
-    printf("Postfix Expression: %s\n", postfix);
-    evaluate(postfix);
+int main()
+{
+    int V, E, i;
+    int adj[MAX][MAX] = {0};  // Adjacency matrix initialized to 0
+    int u, v;
+    
+    printf("Enter the number of routers (vertices): ");
+    scanf("%d", &V);
+    
+    printf("Enter the number of connections (edges): ");
+    scanf("%d", &E);
+    
+    printf("Enter the connections (u v) - routers numbered from 0 to %d:\n", V-1);
+    for (i = 0; i < E; i++)
+    {
+        scanf("%d %d", &u, &v);
+        if (u >= 0 && u < V && v >= 0 && v < V)
+        {
+            adj[u][v] = 1;
+            adj[v][u] = 1;  // Undirected graph
+        }
+        else
+        {
+            printf("Invalid router number! Ignoring edge %d %d\n", u, v);
+        }
+    }
+    
+    findArticulationPoints(adj, V);
+    
     return 0;
 }
